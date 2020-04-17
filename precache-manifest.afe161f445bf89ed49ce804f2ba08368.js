1f996d648bef12b2c514652598a23adb7eea78f1
yogikatba <yogikatba16@gmail.com>
2020-04-17 18:10:01 +0530

    Updates

---
+++ precache-manifest.afe161f445bf89ed49ce804f2ba08368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9943dc43b76086acc4a81d839c6c7f83",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "0b8ddf98629ad339a031",
+    "url": "/react-store/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7c0944b1321ae0418053",
+    "url": "/react-store/static/css/main.9dd228b3.chunk.css"
+  },
+  {
+    "revision": "0b8ddf98629ad339a031",
+    "url": "/react-store/static/js/2.2f528f20.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-store/static/js/2.2f528f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0944b1321ae0418053",
+    "url": "/react-store/static/js/main.2e5de115.chunk.js"
+  },
+  {
+    "revision": "782f84519adf095af0b0",
+    "url": "/react-store/static/js/runtime-main.92ab1a31.js"
+  }
+]);
