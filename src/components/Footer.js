e7e6105aaf311b711add283217cd3c6ca4b9bcd4
Yogi Katba <38407478+yogikatba@users.noreply.github.com>
2020-07-06 12:56:00 +0530

    Footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+export default function Footer() {
+    return (
+        <div>
+            <h4 className='footer text-center'>
+            Made by Yogi Katba
+            </h4>
+        </div>
+    )
+}
